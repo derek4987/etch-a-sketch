c7f42852594eaa0d4b227db7c1322718a3a0646f
Derek Nunes <dereknunes28@gmail.com>
2022-02-15 22:58:39 -0600

    finish createGrid function

--- js-file.js
+++ js-file.js
@@ -1,4 +1,4 @@
-grid();
+createGrid();
 
 document.addEventListener('keydown', function(e) {
     if (e.key === 'Escape') {
@@ -28,11 +28,25 @@ function width() {
     return width;
 }
 
-function grid() {
+function createGrid() {
     /* x is number of squares accross. calculate size of boxes based on 19x9 ratio.
     Add all squares as individual <div>'s.
     Grid <div> class containing all newly imported <div>'s set as flex container with wraping so
     they should fit perfectly if sized correctly.*/
-    const x = width();
-    console.log(x);
+    const x = parseFloat(width());
+    const y = parseFloat(x/(16/9));
+    const numbOfSquares = parseFloat(x * y);
+    const sideLength = parseFloat(Math.sqrt((960*540)/numbOfSquares));
+    console.log(x,y,numbOfSquares);
+
+    for (let i = 1; i <= numbOfSquares; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add('gridSquare');
+        gridSquare.style.width = `${sideLength}px`;
+        gridSquare.style.maxHeight = `${sideLength}px`;
+        gridSquare.style.backgroundColor = 'blue';
+        const container = document.querySelector('.grid');
+        container.appendChild(gridSquare);
+        console.log( gridSquare.style.width);
+    }
 }
